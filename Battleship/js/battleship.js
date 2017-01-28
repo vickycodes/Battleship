ea41dd1df666e81471b1c271dd3df6c784ba298e
Vicky Haub <vicky.haub@gmail.com>
2017-01-28 16:09:45 +0100

    First commit

---
+++ Battleship/js/battleship.js
@@ -0,0 +1,31 @@
+
+
+  var Battlefield = function (row, col){
+    this.matrix = [
+        [0,0,0,1,1,1,1,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,1,0,0,0],
+        [0,0,0,0,0,0,1,0,0,0],
+        [1,0,0,0,0,0,1,1,1,1],
+        [1,0,0,0,0,0,0,0,0,0],
+        [1,0,0,1,0,0,0,0,0,0],
+        [1,0,0,1,0,0,0,0,0,0],
+        [1,0,0,0,0,0,0,0,0,0]
+      ];
+  };
+
+  var computerField = new Battlefield();
+
+
+  Battlefield.prototype.
+  
+
+  Battlefield.prototype.placeShips = function() {
+
+  };
+
+
+  Battlefield.prototype.fireBomb = function() {
+
+  };
